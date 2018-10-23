751e068f66addbb766893fa69244c49cfe13bccf
HockeyWX <HockeyApp@163.com>
2018-10-23 22:04:54 +0800

    Update appcenter-post-build.sh

--- Xamarin.UITest/UITestDemo/Droid/appcenter-post-build.sh
+++ Xamarin.UITest/UITestDemo/Droid/appcenter-post-build.sh
@@ -1,41 +1,4 @@
 #!/usr/bin/env bash
 
-# Post Build Script
-
-set -e # Exit immediately if a command exits with a non-zero status (failure)
-
-echo "**************************************************************************************************"
-echo "Post Build Script"
-echo "**************************************************************************************************"
-
-##################################################
-# Build Xamarin.UITest (For building Xamarin.Android. If you are building Xamarin iOS, please just comment out this part as .sln will run it)
-##################################################
-
-/Library/Frameworks/Mono.framework/Versions/5_12_0/bin/msbuild $APPCENTER_SOURCE_DIRECTORY/Xamarin.UITest/UITestDemo/UITestDemo.UITest/UITestDemo.UITest.csproj /t:Build /p:Configuration=Debug
-
-##################################################
-# Start UI Tests
-##################################################
-
-# variables
-appCenterLoginApiToken=$AppCenterLoginForAutomatedUITests # this comes from the build environment variables
-appName="$AppOwner/$AppName"
-deviceSetName="$AppOwner/$AppDeviceSetName"
-testSeriesName="$AppTestSeriesName"
-
-echo ""
-echo "Start Xamarin.UITest run"
-echo "   App Name: $appName"
-echo " Device Set: $deviceSetName"
-echo "Test Series: $testSeriesName"
-echo ""
-
-echo "> Run UI test command"
-# Note: must put a space after each parameter/value pair
-appcenter test run uitest --app $appName --devices $deviceSetName --app-path $APPCENTER_OUTPUT_DIRECTORY/Pickster.ipa --test-series $testSeriesName --locale "en_US" --build-dir $APPCENTER_SOURCE_DIRECTORY/Pickster.UITests/bin/Debug --uitest-tools-dir $APPCENTER_SOURCE_DIRECTORY/packages/Xamarin.UITest.*/tools --token $appCenterLoginApiToken 
-
-echo ""
-echo "**************************************************************************************************"
-echo "Post Build Script complete"
-echo "**************************************************************************************************"
+cd $APPCENTER_OUTPUT_DIRECTORY
+ls
